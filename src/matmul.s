c1005156bc81f692baca54e2f3ad161709475044
asd88446633 <bb25515522@gmail.com>
2024-11-18 18:24:24 +0800

    Update matmul.s

--- src/matmul.s
+++ src/matmul.s
@@ -116,7 +116,24 @@ inner_loop_start:
     
 inner_loop_end:
     # TODO: Add your own implementation
+    slli t2, a2, 2
+    add  s3, s3, t2            
+    addi s0, s0, 1             
+       
+            
+    j outer_loop_start
 
+outer_loop_end:
+    # Epilogue
+    lw ra, 0(sp)              
+    lw s0, 4(sp)            
+    lw s1, 8(sp)               
+    lw s2, 12(sp)              
+    lw s3, 16(sp)             
+    lw s4, 20(sp)             
+    lw s5, 24(sp)              
+    addi sp, sp, 28           
+    jr ra                     
 error:
     li a0, 38
     j exit
